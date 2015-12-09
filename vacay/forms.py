0396b88a06f6327db652b3acc19e2781d8c4e8b4
kevinwu06 <kevinxwu@gmail.com>
2015-12-08 23:03:52 -0500

    updating add form and home layout.

--- vacay/forms.py
+++ vacay/forms.py
@@ -6,4 +6,5 @@ class AddForm(forms.ModelForm):
 
     class Meta:
         model = UserVacay
-        fields = ('days_away', 'days_here',)
+        fields = ('days_here',)
+        widgets = {'days_here': forms.NumberInput(attrs={'step': 1})}
